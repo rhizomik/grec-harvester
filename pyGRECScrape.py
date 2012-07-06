0cb2cb8345b7646f78994f2985572ba2482f05fa
davidkaste <d.castella.85@gmail.com>
2012-07-06 13:34:00 +0200

    Added colored output and arguments with argparse

--- pyGRECScrape.py
+++ pyGRECScrape.py
@@ -2,11 +2,12 @@
 # -*- coding: utf8 -*-
 
 from bs4 import BeautifulSoup
+from termcolor import colored
 import rdflib
-
 import urllib2
 import re
 import math
+import argparse
 
 def get_soup_from_url(url):
     return BeautifulSoup(urllib2.urlopen(url))
@@ -27,9 +28,9 @@ def get_pub_list_from_link(link):
     max_posts, max_pages = get_pubs_quantity(get_soup_from_url(link))
     link = link+"&PAG=1"
     publication_list = []
-    print "Trobades", max_pages, "pàgines i ", max_posts, "publicacions"
+    print colored("Trobades "+str(max_pages)+" pàgines i "+str(max_posts)+" publicacions", "green")
     for page in range(1, max_pages+1):
-        print "Agafant dades de la pàgina", page
+        print colored("Agafant dades de la pàgina "+ str(page), "blue")
         link = link.replace(re.findall("&PAG=.*", link).pop(), "&PAG="+ str(page))
         pub_page = get_soup_from_url(link)
         publication_list.extend(pub_page.find_all("p", {"class": "llista"}))
@@ -45,10 +46,20 @@ def get_all_pubs_from_link_list(link_list):
 
 def get_pubs_by_row_name(row_name):
     url_obj = 'http://webgrec.udl.es/cgi-bin/DADREC/crcx1.cgi?PID=312186&IDI=CAT&FONT=3&QUE=CRXD&CRXDCODI=1605&CONSULTA=Fer+la+consulta'
-    print "Agafant dades del DIEI a la web del GREC..."
+    print colored("Agafant dades del DIEI a la web del GREC...", "green")
     soup = get_soup_from_url(url_obj)
     link_list = get_links_in_row(soup, row_name)
     return get_all_pubs_from_link_list(link_list)
 
-pubs = get_pubs_by_row_name(".*revistes")
-print "Trobades", len(pubs), "publicacions"
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Little script for scraping data on DIEI GREC website")
+    parser.add_argument("rowtitle",
+        metavar = "\"title\"",
+        help = "The row title (list) that you want to scrap data",
+        type = str,
+        nargs = "+")
+    args = parser.parse_args()
+    print args
+    for row_title in args.rowtitle:
+        pubs = get_pubs_by_row_name(row_title)
+        print "Trobades", len(pubs), "publicacions"
