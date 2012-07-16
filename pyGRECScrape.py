97154dd2d8a3fdb961e116a989be3f8420c1337d
davidkaste <d.castella.85@gmail.com>
2012-07-16 20:04:23 +0200

    Added some funcionality and a little RDF playground ;)

--- pyGRECScrape.py
+++ pyGRECScrape.py
@@ -3,14 +3,16 @@
 
 from bs4 import BeautifulSoup
 
-from rdflib.graph import ConjunctiveGraph, Resource
-from rdflib import Namespace, URIRef, Literal, RDF
+from rdflib.graph import ConjunctiveGraph
+from rdflib import Namespace, URIRef, Literal, RDF, BNode
+from rdflib.collection import Collection
 
 import urllib2
 import simplejson
 import re
 import math
 import argparse
+import unicodedata
 
 #GLOBAL VARS
 pub_base_uri = "http://www.diei.udl.cat"
@@ -20,6 +22,7 @@ swrc = "http://swrc.ontoware.org/ontology#"
 DC = Namespace("http://purl.org/dc/elements/1.1/")
 RDFS = Namespace("http://www.w3.org/2000/01/rdf-schema#")
 UNI = Namespace("http://swrc.ontoware.org/ontology#")
+
 #END GLOBAL VARS
 
 # Create the RDF Graph
@@ -35,6 +38,9 @@ def clean_pub_title(string):
         return string.strip()[:-1]
     return string.strip()
 
+def remove_accents(s):
+    '''Quits accents and language specific characters of a string'''
+    return ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
 
 def clean_href(string):
     '''Clean the javascript stuff from the given content of href property of an anchor'''
@@ -53,7 +59,13 @@ def get_links_in_row(soup, rowname):
     link_list = [a["href"] for a in fila_pubs.find_all("a")]
     return link_list
 
+
+def htmlize_string(string):
+    '''Make a HTML valid string (quits spaces, commas, dots and accents or language specific characters)'''
+    return remove_accents(string.replace(",", "").replace(".", "").replace(" ", ""))
+
 def normalize_author_name(name):
+    '''Normalize an author name to the form "Lastname, F."'''
     # Copied and translated to python from the file PubsRDFizer.java at lines 344 to 414 from rogargon (Roberto García)
     # https://github.com/rogargon/GRECRDFizer/blob/master/src/main/java/net/rhizomik/grecrdfizer/PubsRDFizer.java#L344
     if re.match(".*?(\S\S+)\s+(\S\S?)(?:\s+(\S))?$", name): # Process authornames like Smith J, Doe J R or Frost RA
@@ -80,9 +92,9 @@ def normalize_author_name(name):
         if name.isupper():
             name_list = name.split(" ")
             if len(name_list) == 4 or len(name_list) == 5:
-                return name_list[2].capitalize()+", "+name_list[0][0]+"."+name_list[1][0]+"."
+                name = name_list[2].capitalize()+", "+name_list[0][0]+"."+name_list[1][0]+"."
             if len(name_list) == 3 or len(name_list) == 2:
-                return name_list[1].capitalize()+", "+name_list[0][0]
+                name = name_list[1].capitalize()+", "+name_list[0][0]
         else:
             match = re.match("^(\S+)\s+(:?(\S+)\s+)?(\S+)", name)
             name = match.group(1)[0]+"."
@@ -90,9 +102,11 @@ def normalize_author_name(name):
                 name = match.group(4)+", "+name+match.group(3)[0]+"."
             else:
                 name = match.group(4)+", "+name
+    #graph.add((URIRef(pub_base_uri+"/"+uri_person+"/"+htmlize_string(name)), DC.author, Literal(name)))
     return name
 
 def normalize_author_list(string):
+    '''Normalize an author list to the form "Lastname, F."'''
     if ";" in string:
         stringn = re.split(";| and | i | amb | y ", string)
     elif "," in string:
@@ -115,31 +129,58 @@ def get_pubs_quantity(soup):
     posts_per_page = len(soup.find_all("p", {"class": "llista"}))
     max_pages = int(math.ceil(max_posts / float(posts_per_page)))
     return max_posts, max_pages
+
+
+def rdfize_pub(pub_dict):
+    '''Translate a publication dictionary to a RDF Graph'''
+    pub_uriref = URIRef(pub_base_uri+"/"+uri_pub+"/"+pub_dict["Id. GREC"])
+
+    graph.add((pub_uriref, RDF.type, UNI.Article))
+    graph.add((pub_uriref, DC.date, Literal(pub_dict[u"Any"])))
+    graph.add((pub_uriref, DC.title, Literal(pub_dict[u"Títol"])))
+    graph.add((pub_uriref, UNI.authors, Literal("; ".join(pub_dict[u"Autors"]))))
+    for autor in pub_dict[u"Autors"]:
+        autor_uriref = URIRef(pub_base_uri+"/"+uri_person+"/"+htmlize_string(autor))
+        graph.add((pub_uriref, DC.author, autor_uriref))
+        graph.add((autor_uriref, RDFS.label, Literal(autor)))
+
+    graph.add((pub_uriref, DC.isPartOf, URIRef("Proves")))
+    graph.add((URIRef("Proves"), DC.volume, Literal(23)))
+
+
+def rdfize_pub_list(pub_list):
+    '''Translate the publication list structure to a RDF Graph structure'''
+    for pub_dict in pub_list:
+        rdfize_pub(pub_dict)
+    return graph.serialize(format="pretty-xml")
     
 
 def get_publication_dict(pub_url):
-    ''''''
+    '''Put all the publication info into a Python Dictionary where the keys are the fields'''
     pub_url = get_soup_from_url(pub_url)
     pub_data = pub_url.find_all("b")
     pub_dict = {}
     for item in pub_data:
         if len(item.next_element) < 25:
             titol = clean_pub_title(item.next_element)
-            if titol == "Autors" or titol == "Autor" or titol == "Director":
-                pub_dict[titol] = normalize_author_list(item.next_element.next_element.strip())
-            elif titol == "Nom" or clean_pub_title(item.next_element) == "Organisme":
-                pub_dict[titol] = item.parent.parent.find_all("td")[1].next_element
-            elif titol == "Equip investigador":
-                res_list = [normalize_author_name(nom.next_element.strip()) for nom in item.parent.find_all("a", {"class":"inves"})]
-                pub_dict["Investigador principal"] = res_list[0]
-                pub_dict["Investigadors secundaris"] = res_list[1:]
-            else:
-                pub_dict[titol] = item.next_element.next_element.strip()
-    print pub_dict
+            try:
+                if titol == "Autors" or titol == "Autor" or titol == "Director":
+                    pub_dict[titol] = normalize_author_list(item.next_element.next_element.strip())
+                elif titol == "Nom" or clean_pub_title(item.next_element) == "Organisme":
+                    pub_dict[titol] = item.parent.parent.find_all("td")[1].next_element
+                elif titol == "Equip investigador":
+                    res_list = [normalize_author_name(nom.next_element.strip()) for nom in item.parent.find_all("a", {"class":"inves"})]
+                    pub_dict["Investigador principal"] = res_list[0]
+                    pub_dict["Investigadors secundaris"] = res_list[1:]
+                else:
+                    pub_dict[titol] = item.next_element.next_element.strip()
+            except:
+                pub_dict[titol] = ""
     return pub_dict
 
 
 def get_pub_list_from_link(link):
+    '''Retrieve all the publications from a URL'''
     max_posts, max_pages = get_pubs_quantity(get_soup_from_url(link))
     link = link+"&PAG=1"
     publication_list = []
@@ -155,6 +196,7 @@ def get_pub_list_from_link(link):
 
 
 def get_all_pubs_from_link_list(link_list):
+    '''Retrieve all the publications from a URL list'''
     publication_list = []
     for link in link_list:
         print u"Getting data from", link
@@ -164,6 +206,7 @@ def get_all_pubs_from_link_list(link_list):
 
 
 def get_pubs_by_row_name(row_name):
+    '''Retrieve all the publications by the row title'''
     url_obj = u'http://webgrec.udl.es/cgi-bin/DADREC/crcx1.cgi?PID=312186&IDI=CAT&FONT=3&QUE=CRXD&CRXDCODI=1605&CONSULTA=Fer+la+consulta'
     print "Getting DIEI data from GREC website"
     soup = get_soup_from_url(url_obj)
@@ -178,8 +221,23 @@ if __name__ == '__main__':
         help = "The row title (list) that you want to scrap data",
         type = str,
         nargs = "+")
+    parser.add_argument("-t","--type",
+        help="Type of the output for de harvested data",
+        type=str,
+        default="rdf",
+        choices=["rdf", "json"])
+    parser.add_argument("-f", "--file",
+        help="Name of the file where the output will be written",
+        type=str,
+        required=True)
     args = parser.parse_args()
 
     for row_title in args.rowtitle:
         pubs = get_pubs_by_row_name(row_title)
-        print "Trobades", len(pubs), "publicacions"
+        f = open(args.file, "w")
+        if args.type == "json":
+            f.write(simplejson.dumps(pubs, ensure_ascii = False).encode("utf8"))
+        else:        
+            f.write(rdfize_pub_list(pubs))
+        print "Output written in "+ f.name
+        f.close()
